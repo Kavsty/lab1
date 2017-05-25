f4b5eacecffb14431289e09e75211f3b051abd1a
Kavsty <harrysarkis@mail.ru>
2017-05-25 20:45:46 +0300

    Add files via upload

---
+++ Primes.java
@@ -0,0 +1,26 @@
+public class Primes 
+{
+	public static void main(String[] agrs) 
+	{
+		for (int n=2; n<100; n++)
+		{
+			boolean count;
+			count = isPrime(n);
+			if (count==true)
+			{		
+			System.out.println(n);
+			}
+		}	
+	}
+		public static boolean isPrime(int n) 
+		{
+			for (int i=2; i<n; i++) 
+			{ 
+					if (n % i == 0) 
+					{
+						return false;
+			 		}
+			}
+			return true;
+		}
+}
