a784a8327d225b97e7f28d6eaffe3dfbbdc2d06b
Kavsty <harrysarkis@mail.ru>
2017-06-29 17:43:18 +0300

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,21 +1,31 @@
 public class Palindrome
 {
-	public static void main(String[] agrs)
+	public static void main(String[] args)
 	{
-		String in=" ";
-		char r = Slovo(in);
-		for (int count=0; count<agrs.length; count++)
-		{
-			in = agrs[count];
+	        Scanner in = new Scanner(System.in);
+	        System.out.println("Введите слово или строку");
+	        String s = in.next();
+	        isPalindrome(s);
+	}
+	    public static String reverseString(String s)
+	    {
+	        String r = "";
+	        for (int i = s.length() - 1; i >= 0; --i)
+	            r += s.charAt(i);
+	        return r;
+	          
+	    }
+	    public static Boolean isPalindrome(String s) 
+	    {
+	        if(s.equals(reverseString(s)))
+	        {
+	            System.out.println("Полиндром");
+	        }
+	        else
+	        {
+	            System.out.println("Не полиндром");
+	        }
+	        return s.equals(reverseString(s));
+	        
 		}
-		System.out.println("Vvod" + in);
-		System.out.println("Znachenie=" + r);
-	}
-	public static String Slovo (String in)
-	{
-		char result;
-		result = in.charAt(3);
-		System.out.println(result);	
-		return result;
-	}
 }
